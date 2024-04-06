c0e74ae861d8ed3308495e2ebcdda814d51d852d
Kevin <kevinzhang882019@gmail.com>
2024-04-06 16:37:01 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    async function fetchFromCity(query: string): Promise<Object> {
+        let url = `http://api.weatherapi.com/v1/current.json?key=683402e6138b4628989215433240604&q=${query}&aqi=yes`;
+        const jsonData = await getJSONFromURL(url);
+        return jsonData;
+    }
+
+    async function getZipcodeByIP(): Promise<string> {
+        let url =
+            "https://geolocation-db.com/json/0daad5e0-82e7-11ee-92e0-f5d620c7dcb4";
+        const jsonData = await getJSONFromURL(url);
+
+        // console.log("geolocation", jsonData);
+        return jsonData.postal;
+    }
+
+    async function getJSONFromURL(url: string) {
+        const response = await fetch(url, {
+            method: "GET",
+            mode: "cors",
+        });
+        const jsonData = await response.json();
+        return jsonData;
+    }
+
+    let weatherData: any = null;
+
+    onMount(async () => {
+        let zipcode = await getZipcodeByIP();
+        weatherData = await fetchFromCity(zipcode);
+        console.log(weatherData);
+    });
+</script>
+
+<main>
+    {#if weatherData != null}
+        <div>Weather</div>
+    {:else}
+        <div class="loader"></div>
+    {/if}
+</main>
+
+<style>
+    .loader {
+        border: 16px solid #f3f3f3;
+        border-radius: 50%;
+        border-top: 16px solid #2895b6;
+        width: 80px;
+        height: 80px;
+        -webkit-animation: spin 1s linear infinite; /* Safari */
+        animation: spin 1s linear infinite;
+    }
+
+    /* Safari */
+    @-webkit-keyframes spin {
+        0% {
+            -webkit-transform: rotate(0deg);
+        }
+        100% {
+            -webkit-transform: rotate(360deg);
+        }
+    }
+
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+</style>
